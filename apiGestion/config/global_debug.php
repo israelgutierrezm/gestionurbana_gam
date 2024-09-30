aee7f8eff75167b1d272b5f15cb48e979472c0e8
marcoreyesglez <marcoreyesglez@gmail.com>
2024-09-30 10:09:27 -0600

    Update global_debug.php

--- apiGestion/config/global_debug.php
+++ apiGestion/config/global_debug.php
@@ -1,6 +1,6 @@
 <?php
 
-$GLOBALS['url_front'] = 'http://localhost/gestionurbana_gam/';
+$GLOBALS['url_front'] = 'http://localhost/gestionurbana_gam/frontGestionGAM/src/';
 $GLOBALS['url_front_pago']="http://localhost:4000/";
 $GLOBALS['url_front_assets']="http://localhost/gestionurbana_gam/";
 
